aa08b38ceffa80a80cd426968c7b8fccdf7854a4
Stan <s2000ProMax@gmail.com>
2022-02-22 20:29:24 +0300

    Minimal config

---
+++ readme.md
@@ -0,0 +1,8 @@
+Initial project:
+npm init -y
+
+Install webpack: https://webpack.js.org/
+npm install -D webpack webpack-cli
+
+Create file:
+webpack.config.js
---
+++ src/analytics.js
@@ -0,0 +1,25 @@
+function createAnalytics() {
+  let counter = 0;
+  let isDestroyed = false;
+
+  const listener = () => counter++;
+
+  document.addEventListener('click', listener);
+
+  return {
+    destroy() {
+      document.removeEventListener('click', listener);
+      isDestroyed = true;
+    },
+
+    getClicks() {
+      if (isDestroyed) {
+        return 'Analytics was destroyed';
+      }
+      return counter;
+    }
+
+  }
+}
+
+window.analytics = createAnalytics();
